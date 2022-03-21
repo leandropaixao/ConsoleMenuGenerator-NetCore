33498ead0431cb2e3637ad5325b9d0541ffc8893
Leandro Paixão <leandro.tec.inf@gmail.com>
2022-03-21 13:01:13 -0400

    First Commit

---
+++ ConsoleMenuGenerator/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleMenuGenerator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Salut monde");
+        }
+    }
+}
+
