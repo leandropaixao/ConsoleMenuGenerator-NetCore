626e6c37537c9ffa0fcff5fbf2e75d9b8a929ba4
Leandro Paixão <leandro.tec.inf@gmail.com>
2022-03-22 13:02:48 -0400

    Ajouté les validations du console: dotnet run

--- ConsoleMenuGenerator/Program.cs
+++ ConsoleMenuGenerator/Program.cs
@@ -4,12 +4,70 @@ namespace ConsoleMenuGenerator
 {
     class Program
     {
-        public static void Main(string[] args)
+       public static void Main(string[] args)
         {
-            if (string.IsNullOrEmpty(args))
+            if (args.Count() == 0)
             {
-                Console.WriteLine("Salut monde");
-            }                
+                MessageHelp();
+                return;
+            }
+            
+            var path = args[0];
+
+            if (!File.Exists(path))
+            {
+                InvalidPath();
+                return;
+            }
+
+            var extension = Path.GetExtension(path);
+
+            if (!extension.Equals(".proto") && !extension.Equals(".json"))
+            {
+                InvalidExtension();
+                Console.WriteLine(extension);
+                return;
+            }
+
+        }
+
+        private static string GetProjectGithub()
+        {
+            return "Projeto GitHub: https://github.com/leandropaixao/ConsoleMenuGenerator-NetCore";
+        }
+
+        private static void MessageHelp()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(GetProjectGithub());
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Utilização: dotnet run [argumento]");
+            Console.WriteLine("");
+            Console.WriteLine("  argumento:");
+            Console.WriteLine("    - caminho do arquivo .json ou do arquivo .proto");
+            Console.WriteLine("");
+            Console.ResetColor();
+        }
+
+        private static void InvalidPath()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(GetProjectGithub());
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Caminho do arquivo inválido ou arquivo inexistente.");
+            Console.ResetColor();
+        }
+
+        private static void InvalidExtension()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(GetProjectGithub());
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Tipo de arquivo inválido, deve ser .json ou .proto.");
+            Console.ResetColor();
         }
     }
 }
