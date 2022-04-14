38cbde4770c3e0b6a9e428af1c8d0ca9ea0c24a6
Leandro Paixão <leandro.tec.inf@gmail.com>
2022-04-14 12:45:00 -0400

    Éléve le méthode menuconsole()

--- ConsoleMenuGenerator/Program.cs
+++ ConsoleMenuGenerator/Program.cs
@@ -80,75 +80,7 @@ namespace ConsoleMenuGenerator
             else
                 Console.WriteLine("Caminho do arquivo inválido ou arquivo inexistente.");
             Console.ResetColor();
-        }
-
-        private static void MenuConsole()
-        {
-            Console.Clear();
-            var sair = false;
-            var opcao = "";
-            do
-            {                 
-                Console.ForegroundColor = ConsoleColor.DarkBlue;
-                Console.WriteLine("+----- Menu console -----+");
-                Console.WriteLine("| 1 - Opcao 1            |");
-                Console.WriteLine("| 2 - Opcao 2            |");
-                Console.WriteLine("| 3 - Opcao 3            |");
-                Console.WriteLine("| 4 - Opcao 4            |");
-                Console.WriteLine("| 5 - Opcao 5            |");
-                Console.WriteLine("+------------------------+");
-                Console.WriteLine("| S - Sair               |");
-                Console.WriteLine("+------------------------+");
-                Console.ResetColor();
-
-                Console.Write("Informe um valor: ");
-                opcao = Console.ReadLine();                             
-                {
-                    var number = 0;
-                    var isNumber = int.TryParse(opcao, out number);
-
-                    if (isNumber)
-                    {
-                        switch(number)
-                        {
-                            case 1 : 
-                                Console.WriteLine("Escolhida a opcao 1");
-                                break;
-                            case 2 : 
-                                Console.WriteLine("Escolhida a opcao 2");
-                                break;
-                            case 3 : 
-                                Console.WriteLine("Escolhida a opcao 3");
-                                break;
-                            case 4 : 
-                                Console.WriteLine("Escolhida a opcao 4");
-                                break;
-                            case 5 : 
-                                Console.WriteLine("Escolhida a opcao 5");
-                                break;
-                            default:
-                                Console.WriteLine();
-                                Console.WriteLine("Opção inválida");
-                                break;
-                        }
-
-                    }
-                    else
-                    {
-                        if (opcao.Equals("S"))
-                        {
-                            sair = true;
-                            Console.WriteLine("Saindo do sistema");
-                        }
-                        else
-                        {
-                            Console.Clear();
-                            Console.WriteLine("Opçào inválida. Informe outra opção");
-                        }
-                    }
-                }
-            } while (!sair);            
-        }
+        }        
     }
 }
 
